0e799750b54201346ab92a308d9c739a0731ffb3
wellington.siqueira <wellingtonex@gmail.com>
2017-01-22 23:23:26 -0300

    implementando listagem de produtos

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -1,8 +1,11 @@
 package br.com.casadocodigo.loja.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
@@ -22,10 +25,18 @@ public class ProdutosController {
 		return modelAndView;
 	}
 	
-	@RequestMapping("produtos")
+	@RequestMapping(value="produtos", method=RequestMethod.POST)
 	public String grava(Produto produto) {
 		System.out.println(produto);
 		produtoDAO.gravar(produto);
 		return "produtos/ok";
+	}
+	
+	@RequestMapping(value="produtos", method=RequestMethod.GET)	
+	public ModelAndView listar() {
+		ModelAndView modelAndView = new ModelAndView("produtos/lista");
+		List<Produto> produtos = produtoDAO.listar();
+		modelAndView.addObject("produtos", produtos);
+		return modelAndView;
 	}
 }
