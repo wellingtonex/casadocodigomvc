c61caa24000c058e432a0ced609e9ace250606cf
wellington.siqueira <wellingtonex@gmail.com>
2017-01-26 11:16:37 -0300

    trabalhado com validações, primeira parte

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -2,8 +2,13 @@ package br.com.casadocodigo.loja.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,34 +17,46 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.models.TipoPreco;
+import br.com.casadocodigo.loja.validation.ProdutoValidation;
 
 @Controller
 @RequestMapping("produtos")
 public class ProdutosController {
-	
+
 	@Autowired
 	private ProdutoDAO produtoDAO;
+
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.addValidators(new ProdutoValidation());
+	}
 
 	@RequestMapping("form")
 	public ModelAndView form() {
 		ModelAndView modelAndView = new ModelAndView("produtos/form");
-		modelAndView.addObject("tipos", TipoPreco.values());		
+		modelAndView.addObject("tipos", TipoPreco.values());
 		return modelAndView;
 	}
-	
-	@RequestMapping(method=RequestMethod.POST)
-	public String grava(Produto produto, RedirectAttributes redirectAttributes) {
-		System.out.println(produto);
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView grava(@Valid Produto produto, BindingResult result,
+			RedirectAttributes redirectAttributes) {
+
+		if (result.hasErrors()) {
+			return form();
+		}
+
 		produtoDAO.gravar(produto);
-		redirectAttributes.addFlashAttribute("sucesso", "Produto cadastrado com sucesso!");		
-		return redirect("produtos");
+		redirectAttributes.addFlashAttribute("sucesso",
+				"Produto cadastrado com sucesso!");
+		return new ModelAndView(redirect("produtos"));
 	}
 
 	private String redirect(String link) {
 		return "redirect:".concat(link);
 	}
-	
-	@RequestMapping(method=RequestMethod.GET)	
+
+	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView listar() {
 		ModelAndView modelAndView = new ModelAndView("produtos/lista");
 		List<Produto> produtos = produtoDAO.listar();
---
+++ src/main/java/br/com/casadocodigo/loja/validation/ProdutoValidation.java
@@ -0,0 +1,28 @@
+package br.com.casadocodigo.loja.validation;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import br.com.casadocodigo.loja.models.Produto;
+
+public class ProdutoValidation implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return Produto.class.isAssignableFrom(clazz);
+	}
+
+	@Override
+	public void validate(Object object, Errors errors) {
+		ValidationUtils.rejectIfEmpty(errors, "titulo", "field.required");
+		ValidationUtils.rejectIfEmpty(errors, "descricao", "field.required");
+		
+		Produto produto = (Produto) object;
+		
+		if(produto.getPaginas() <= 0){
+			errors.rejectValue("paginas", "field.required");
+		}
+		
+	}
+}
