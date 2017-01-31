60a07b46ca21f62d01df58b0fc215cf4549ed19e
wellington.siqueira <wellingtonex@gmail.com>
2017-01-31 11:17:31 -0300

    configurando permissoes do spring security

--- src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
@@ -1,9 +1,23 @@
 package br.com.casadocodigo.loja.conf;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 @EnableWebMvcSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	    http.authorizeRequests()
+	    .antMatchers("/produtos/form").hasRole("ADMIN")
+	    .antMatchers("/carrinho/**").permitAll()
+	    .antMatchers(HttpMethod.POST, "/produtos").hasRole("ADMIN")
+	    .antMatchers(HttpMethod.GET, "/produtos").hasRole("ADMIN")
+	    .antMatchers("/produtos/**").permitAll()
+	    .antMatchers("/").permitAll()
+	    .anyRequest().authenticated()
+	    .and().formLogin();
+	}
 }
