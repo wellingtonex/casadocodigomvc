620cbe57d6461cc3eabdeabb187348dd9158b65c
wellington.siqueira <wellingtonex@gmail.com>
2017-01-30 21:53:38 -0300

    retornando json

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -83,5 +84,11 @@ public class ProdutosController {
 	    Produto produto = produtoDAO.find(id);
 	    modelAndView.addObject("produto", produto);
 	    return modelAndView;
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	@ResponseBody
+	public Produto detalheJSON(@PathVariable("id") Long id){
+	    return produtoDAO.find(id);
 	}
 }
