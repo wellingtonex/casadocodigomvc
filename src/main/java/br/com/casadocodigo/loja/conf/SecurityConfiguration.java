6192431e53484e1e84b6cfc1499a06efa855f7b1
wellington.siqueira <wellingtonex@gmail.com>
2017-02-01 11:37:29 -0300

    removento a anotação @EnableWebMvcSecurity que está depreciada por @EnableWebSecurity em SecurityConfiguration

--- src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
@@ -4,14 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import br.com.casadocodigo.loja.daos.UsuarioDAO;
 
-@EnableWebMvcSecurity
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
 	@Autowired
