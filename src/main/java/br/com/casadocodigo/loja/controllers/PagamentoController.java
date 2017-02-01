84a6dbfd604ec6581f50f9da3e91841adc7aa393
wellington.siqueira <wellingtonex@gmail.com>
2017-02-01 18:42:55 -0300

    enviando email de confirmação de contra

--- src/main/java/br/com/casadocodigo/loja/controllers/PagamentoController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/PagamentoController.java
@@ -4,6 +4,9 @@ import java.util.concurrent.Callable;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +18,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.casadocodigo.loja.models.CarrinhoCompras;
 import br.com.casadocodigo.loja.models.DadosPagamento;
+import br.com.casadocodigo.loja.models.Usuario;
 
 @Controller
 @RequestMapping("/pagamento")
@@ -26,15 +30,19 @@ public class PagamentoController {
 	
 	@Autowired
 	private RestTemplate restTemplate;
+	
+	@Autowired
+	private MailSender sender;
 
 	@RequestMapping(value="/finalizar", method=RequestMethod.POST)
-	public Callable<ModelAndView> finalizar(RedirectAttributes model) {
+	public Callable<ModelAndView> finalizar(@AuthenticationPrincipal Usuario usuario, RedirectAttributes model) {
 		
 		return () -> {
 			try {
 				String uri = "http://book-payment.herokuapp.com/payment";
 				String response = restTemplate.postForObject(uri, new DadosPagamento(carrinho.getTotal()), String.class);
 				System.out.println(response);
+				enviEmailCompraProduto(usuario);
 				model.addFlashAttribute("sucesso", "Pagamento realizado com sucesso.");			
 			} catch (HttpClientErrorException e) {
 				e.printStackTrace();
@@ -46,5 +54,16 @@ public class PagamentoController {
 			return modelAndView;			
 		};
 		
+	}
+
+	private void enviEmailCompraProduto(Usuario usuario) {
+		SimpleMailMessage email = new SimpleMailMessage();
+		email.setSubject("Compra finalizada com sucesso");
+		//email.setTo(usuario.getEmail());
+		email.setTo("wellingtonex@gmail.com");
+		email.setText("Compra aprovada com sucesso no valor de " + carrinho.getTotal());
+		email.setFrom("alura.springmvc@gmail.com");
+		
+		sender.send(email);
 	}
 }
