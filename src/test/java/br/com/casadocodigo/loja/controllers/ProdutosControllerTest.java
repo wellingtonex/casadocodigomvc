063c94cd5659c13d476ab27db86dded82a1f920a
wellington.siqueira <wellingtonex@gmail.com>
2017-02-01 14:58:08 -0300

    adicionando o filtro do spring security aos testes de controllers

--- src/test/java/br/com/casadocodigo/loja/controllers/ProdutosControllerTest.java
+++ src/test/java/br/com/casadocodigo/loja/controllers/ProdutosControllerTest.java
@@ -1,9 +1,12 @@
 package br.com.casadocodigo.loja.controllers;
 
+import javax.servlet.Filter;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -17,28 +20,45 @@ import org.springframework.web.context.WebApplicationContext;
 import br.com.casadocodigo.loja.conf.AppWebConfiguration;
 import br.com.casadocodigo.loja.conf.DataSourceConfigurationTest;
 import br.com.casadocodigo.loja.conf.JPAConfiguration;
+import br.com.casadocodigo.loja.conf.SecurityConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @ContextConfiguration(classes = { JPAConfiguration.class,
-		AppWebConfiguration.class, DataSourceConfigurationTest.class })
+		AppWebConfiguration.class, DataSourceConfigurationTest.class,
+		SecurityConfiguration.class })
 @ActiveProfiles("test")
 public class ProdutosControllerTest {
 
 	@Autowired
 	private WebApplicationContext webContext;
 
+	@Autowired
+	private Filter springSecurityFilterChain;
+
 	private MockMvc mockMvc;
 
 	@Before
 	public void setup() {
-		this.mockMvc = MockMvcBuilders.webAppContextSetup(webContext).build();
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(webContext)
+				.addFilter(springSecurityFilterChain).build();
 	}
 
 	@Test
 	public void deveRetornarParaHomeComOsLivros() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/"))
-				.andExpect(MockMvcResultMatchers.model().attributeExists("produtos"))
-				.andExpect(MockMvcResultMatchers.forwardedUrl("/WEB-INF/views/home.jsp"));
+				.andExpect(MockMvcResultMatchers.model()
+						.attributeExists("produtos"))
+				.andExpect(MockMvcResultMatchers
+						.forwardedUrl("/WEB-INF/views/home.jsp"));
+	}
+
+	@Test
+	public void somenteAdminDeveAcessarProdutosForm() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/produtos/form")
+				.with(SecurityMockMvcRequestPostProcessors
+						.user("user@casadocodigo.com.br").password("123456")
+						.roles("USUARIO")))
+				.andExpect(MockMvcResultMatchers.status().is(403));
 	}
 }
