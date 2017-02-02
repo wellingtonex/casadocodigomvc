93529e29d52477e06303871963b101a35c950eb3
wellington.siqueira <wellingtonex@gmail.com>
2017-02-02 15:29:42 -0300

    corrigindo profile de producao

--- src/main/java/br/com/casadocodigo/loja/conf/JPAProductionConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/JPAProductionConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-@Profile("dev")
+@Profile("prod")
 public class JPAProductionConfiguration {
 	
 	@Autowired
